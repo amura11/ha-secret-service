dc346b6d64874250a3cccc006646e50bc5135b7a
AndrewQ <amura.cxg@gmail.com>
2023-07-27 22:58:09 -0600

    Fixed missing field issue and added reload support - Fixed an issue where the integration would crash if the groups or secrets fields weren't specified (#6) - Added code to handle when HA reloads the configuration to reload the integration configuration (#7) - Added some more logging

--- custom_components/secret_service/__init__.py
+++ custom_components/secret_service/__init__.py
@@ -9,7 +9,10 @@ import bcrypt
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 
+from homeassistant.helpers.reload import async_integration_yaml_config
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.const import SERVICE_RELOAD
 from homeassistant.core import (
     HomeAssistant,
     ServiceCall,
@@ -19,6 +22,7 @@ from homeassistant.core import (
 
 from .const import (
     DOMAIN,
+    SETUP_KEY,
     LOGGER,
     ATTR_GROUP,
     ATTR_GROUPS,
@@ -27,11 +31,9 @@ from .const import (
     ATTR_NAME,
     ATTR_VALUE,
     ATTR_FULL_RESPONSE,
+    SERVICE_CHECK_SECRET,
     ValidateResult,
 )
-
-
-# from homeassistant.const import ()
 
 SECRET_SCHEMA = vol.Schema(
     {
@@ -73,13 +75,30 @@ CHECK_SECRET_SCHEMA = vol.Schema(
 )
 
 
-def setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Initialize the Secret Service."""
-    LOGGER.debug("Setting up secret service")
-    service_config: ConfigType = config[DOMAIN]
-    service = SecretValidatorService(service_config)
+async def async_setup(hass: HomeAssistant, config: ConfigEntry) -> bool:
+    """Initialize the integration and register the services."""
+    LOGGER.debug("Beginning setup")
 
-    def handle_check_secret(call: ServiceCall) -> ServiceResponse:
+    if SETUP_KEY in hass.data:
+        LOGGER.debug("Setup key present, skipping setup")
+        return True
+
+    service: SecretValidatorService = SecretValidatorService(config[DOMAIN])
+    hass.data[SETUP_KEY] = True
+
+    # Service handlers
+    async def async_handle_reload_entry() -> None:
+        LOGGER.debug("Reloading config")
+        new_config: ConfigEntry | None = await async_integration_yaml_config(
+            hass, DOMAIN
+        )
+
+        if (new_config is None) or (DOMAIN not in new_config):
+            raise ValueError("A valid config could not be found")
+
+        service.reload(new_config[DOMAIN])
+
+    async def async_handle_check_secret(call: ServiceCall) -> ServiceResponse:
         name: str = call.data.get(ATTR_NAME)
         value: str = call.data.get(ATTR_VALUE)
         use_full_response = call.data.get(ATTR_FULL_RESPONSE, False)
@@ -92,10 +111,12 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
         else:
             return {"result": result == ValidateResult.SUCCESS}
 
+    # Register handlers
+    hass.services.async_register(DOMAIN, SERVICE_RELOAD, async_handle_reload_entry)
     hass.services.async_register(
         DOMAIN,
-        "check_secret",
-        handle_check_secret,
+        SERVICE_CHECK_SECRET,
+        async_handle_check_secret,
         CHECK_SECRET_SCHEMA,
         SupportsResponse.ONLY,
     )
@@ -106,41 +127,54 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
 class SecretValidatorService:
     """A class that stores secret/group configurations and can validate secrets."""
 
-    _groupValidators: dict[str, SecretGroupValidator]
-    _individualValidators: dict[str, SecretValidator]
+    _group_validators: dict[str, SecretGroupValidator]
+    _individual_validators: dict[str, SecretValidator]
 
     def __init__(self, service_config: ConfigType) -> None:
         """Initialize the SecretValidatorService class."""
-        group_configs: list[ConfigType] = service_config.get(ATTR_GROUPS)
-        secret_configs: list[ConfigType] = service_config.get(ATTR_SECRETS)
+        self._group_validators = {}
+        self._individual_validators = {}
+        self._load_config(service_config)
 
-        self._groupValidators = {}
-        self._individualValidators = {}
-
-        for group_config in group_configs:
-            group_name = group_config.get(ATTR_GROUP)
-            self._groupValidators[group_name] = SecretGroupValidator(group_config)
-
-        for secret_config in secret_configs:
-            secret_name = secret_config.get(ATTR_SECRET)
-            self._individualValidators[secret_name] = SecretValidator(secret_config)
+    def reload(self, service_config: ConfigType) -> None:
+        """Reload the service using the given configuration."""
+        self._group_validators.clear()
+        self._individual_validators.clear()
+        self._load_config(service_config)
 
     def validate(self, name: str, value: str) -> ValidateResult:
         """Validate the provided value against the stored secrets/groups."""
         result: ValidateResult = ValidateResult.FAILED_INVALID
 
-        if name in self._individualValidators:
+        if name in self._individual_validators:
             LOGGER.debug("Name %s matches a single secret, validating", name)
-            result = self._individualValidators[name].validate(value)
-        elif name in self._groupValidators:
+            result = self._individual_validators[name].validate(value)
+        elif name in self._group_validators:
             LOGGER.debug("Name %s matches a group of secrets, validating", name)
-            result = self._groupValidators[name].validate(value)
+            result = self._group_validators[name].validate(value)
         else:
             LOGGER.debug("Name %s does not match any secrets", name)
 
         LOGGER.debug("Validation result: %s", result)
 
         return result
+
+    def _load_config(self, service_config: ConfigType) -> None:
+        LOGGER.debug("Loading config")
+        group_configs: list[ConfigType] | None = service_config.get(ATTR_GROUPS)
+        secret_configs: list[ConfigType] | None = service_config.get(ATTR_SECRETS)
+
+        if group_configs:
+            for group_config in group_configs:
+                group_name = group_config.get(ATTR_GROUP)
+                self._group_validators[group_name] = SecretGroupValidator(group_config)
+
+        if secret_configs:
+            for secret_config in secret_configs:
+                secret_name = secret_config.get(ATTR_SECRET)
+                self._individual_validators[secret_name] = SecretValidator(
+                    secret_config
+                )
 
 
 class SecretGroupValidator:
@@ -159,11 +193,12 @@ class SecretGroupValidator:
         self._salt = bcrypt.gensalt()
         self._validators = {}
 
-        for secret_config in secret_configs:
-            # Hash the secret using the group id so we can easily lookup the scret checker
-            secret_value: str = secret_config.get(ATTR_VALUE)
-            key = self._generate_secret_key(secret_value)
-            self._validators[key] = SecretValidator(secret_config)
+        if secret_configs:
+            for secret_config in secret_configs:
+                # Hash the secret using the group id so we can easily lookup the validator
+                secret_value: str = secret_config.get(ATTR_VALUE)
+                key = self._generate_secret_key(secret_value)
+                self._validators[key] = SecretValidator(secret_config)
 
     def validate(self, value: str) -> ValidateResult:
         """Validate the provided value against the secrets in this group."""
